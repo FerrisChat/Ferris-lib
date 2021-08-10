79f168aa44daf1cff0de9df16b7fc617ec37c767
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2021-08-09 20:04:35 -0400

    Re-did RequestHandler
    Unfortunately the Api is down so not much testing could be done

--- lib/rest/RequestHandler.ts
+++ lib/rest/RequestHandler.ts
@@ -9,29 +9,57 @@ export class RequestHandler {
 
     constructor(client: any) {
 
-        this.baseUrl = Urls.Api + Urls.Base_Api + API_VERSION
+        this.baseUrl = "https://fallback-api.ferrischat.hydrostaticcog.me" + Urls.Base_Api + API_VERSION
         this.userAgent = `FerrisLib (https://github.com/Drxckzyz/Ferris-lib, ${require('../../package.json').version})`
         //this is a test token to be able interact with the api
-        this.tempAuth = "MA==.p1TqYEW0GtJZV4nzQkyqf8oCyO3ZJiwh_2GwijO8P3O4G3UQ4bIBoq1KZYA8rB4gRFS3mlIzxCua3gwBIa7RZpVDTFLkwlBqRwylz2yxwaWJ9_JjqyvMCK8bp0paHGliDYGFvmx08pySTT-qC-UU6lhpjA-LRUeer4MwEGaPHyRpZTEkiQHuedUDEMczxKGJSmwrBAN809GpnxgoJt-zK9WVXp3GGSJu4jkGOBGNQRTQBgAlkNxysUUOImVbzWmVAT-iw7AMwhI70im0Noj0PP47MhkskMZhnzVG2755BH8IzC2-BcheamRWYkEyaNNa4dx7Qf4mdlsbi0EFjS0kkQ=="
+        this.tempAuth = "MA==.2gwqJT9ODJTDjwv3sfRHt0XN7NVs_B4vJLqWqKO3YBf_QDUoGJbrl1JUAhpTCd1kGV_bLNaKCI7J74YE8dRXuSZmo7yWez4UBhD4RQ55PD_5hyJsXCvt8AS19VA5KNdksFWmqpieMykSJ6P5NKOqTJKffttXwELe5WXFg_R5ZBK63HvIQifmaWXMwER-FqffcFGMyhZeA25pXIUCm-yD8thDNMy9T90Viz7nzEfiLJmKeNM8ymf6JqZjqok9txjmt201FUNQxwlPOoQOvnki-Yfj3WWxZ_3I0fo0LfEStsrs_2iqQLru0FK2hY-MdCOajNDz5V1X4MjS2_USqJ8sAw=="
     }
 
     async request(method: RequestMethods, url: string, body?: any) {
         return new Promise(async (resolve, reject) => {
             const headers = {
                 "User-Agent": this.userAgent,
-                "Authorization": this.tempAuth
+                "Authorization": this.tempAuth,
+                "Content-type": "application/json"
             };
 
-            console.log(this.baseUrl + url)
+            const finalURL = this.baseUrl + url
 
-            const req = await fetch(this.baseUrl + url, {
+            const controller = new AbortController()
+            const timeout = setTimeout(() => controller.abort(), 1000 * 30)
+
+            const res = await fetch(finalURL, {
                 headers,
                 body,
-            })
+                signal: controller.signal,
+                method,
+            }).finally(() => clearTimeout(timeout))
 
-            const data = await req.json()
+            if (res.ok) {
+                resolve(this.parseResponse(res))
+            }
 
-            resolve(data)
+            if (res.status === 401 || res.status === 403) {
+                console.log("Invalid Something")
+            } if (res.status >= 400 && res.status <= 500) {
+                if (res.status === 429) return console.warn("Ratelimit")
+                else if (res.status === 404) return console.warn("Whatever was requested was not found")
+
+                let data;
+                try {
+                    data = await this.parseResponse(res)
+                } catch (error) {
+                    //reject(error)
+                    console.error("Malformed Response")
+                }
+
+                console.error("Throw Some Api Error Here")
+            } else if (res.status >= 500 && res.status < 600) {
+                console.warn("Server Issuses")
+            }
         })
+    }
+    private parseResponse(res) {
+        return res.json();
     }
 }
--- test/test.js
+++ test/test.js
@@ -4,3 +4,9 @@ const http69 = new Ferrislib.RequestHandler(null)
 //Test user: 4398046511104
 //Test guild: 8796093022208
 http69.request("GET", Ferrislib.Constants.Endpoints.USER("4398046511104"))
+    .then((req) => {
+        console.log(req)
+    })
+    .catch((e) => {
+        console.error(e)
+    })
