b58d8b30cd3913df7847cf0a406ef4d73b49c6e7
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2021-08-21 13:21:45 -0400

    More Docs Commenting

--- lib/models/Base.ts
+++ lib/models/Base.ts
@@ -2,24 +2,42 @@ import { Client } from "../Client";
 
 /**
  * The base class for all the models
- * @param id
+ * @param {string} id of the Model
  */
 export default class Base {
-    id: string;
+    Id: string;
 
     constructor(id: string) {
 
-        this.id = id
+        /**
+         * The Id of the Model
+         * @type {string}
+         */
+        this.Id = id
     }
 
+    /**
+     * Clones the Model
+     * @returns A clone of the Model
+     */
     _clone() {
         return Object.assign(Object.create(this), this);
     }
 
+    /**
+     * Updates the model data
+     * @param {any} data 
+     * @returns The updated Data
+     */
     _patch(data) {
         return data;
     }
 
+    /**
+     * Updates the Moedl data
+     * @param {any} data 
+     * @returns The old data of the model
+     */
     _update(data) {
         const clone = this._clone();
         this._patch(data);
--- lib/models/Channel.ts
+++ lib/models/Channel.ts
@@ -3,10 +3,22 @@ import Base from "./Base";
 
 /**
  * The Channel Model
+ * @param {any} data The raw channel data
+ * @param {Client} client The Client
  */
 export class Channel extends Base {
+    /**
+     * The name of the channel
+     * @type {string}
+     */
     public name: string
+
+    /**
+     * The client for this channel
+     * @type {Client}
+     */
     #_client: Client;
+
     constructor(data: any, client: Client) {
         super(data.id);
 
