ce28fb374bf4b301d33a78ef75eacf8fe20f1b89
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2021-09-06 16:56:30 -0400

    Server got reset

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 const Ferrislib = require('../build')
-const token = "OTU3ODU3NjAxMzE3Mzk4OTU4Njk5NjE0MDQ0MTYw.51vcGt4fTMzgqYsFdWAufi1jibaW4swIQxul8H5QsoED-K5L_esPiA8nvvRmHx4P8w3tpImQpm_yihjg3RYMXmqHwWJpP0n0gRTqSCZIDgkB9B1vcTbKTZjtxs54UbnZUajju9QtooE9uWM9R7e0X0Xl9e4MXBo6nhl4VrryEu0kKOQWdo0X-ce_Ks3sxHBas4qOr5xOCAdpCaHwU5C_VhZ8nXa9Q-g_t_lEYHYuBNfmswU4tfA9iWldQW-I5-sKPy5T1JNXBIifikszAtoou7p_hCU56sFBfvXrO8D_5uDIsV72tKOJb_Hw5vm-MNMScwUkpQn_7lkr1FTdRNkOaA=="
+const token = "OTc5OTc1Mjc1MzkwMTQ3MjUxODc3NTk4OTg2MjQw.oPv_AsjrLgMwF4QD2BTWkvYGfZMopTvILZM7NVSAkvqI_dWNYlofF9RRbD_9wn4PYskwDXHhgNkPmjUpS3GlsRYeqxS3plXmWwlSqPWsLwPyvSJjrT-UD03cf3BXE0oX4eSgE9PyrQOQrypdBIIzjJoziPLC2WpwnANuPVtqkPlmSRS7nmsAJBhkStUzqOOfX9pZIoCl4knt1ppOalhYgvpZ8EmfX4Nbu31Oc331lOeOQUGJEuIo-0YHyEmBTOFqw0CIJcjaoGXfP8lO0G-vLClR_EIRLLMNEcyKYb1pWMp0FrfrzKMBTklvVRxePwRFHl0mzg7I7HCUxmRQZcM8Zw=="
 const sys = new Ferrislib.Client(token, {
     shardCount: 1,
 })
-
 /** */
 async function test() {
     sys.on("debug", console.log)
