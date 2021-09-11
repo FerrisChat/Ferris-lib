28fdfc75ec3a9ee72970d92f813ca5eda82260b2
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2021-09-11 16:17:27 -0400

    Direct import

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const Ferrislib = require('../build')
+const Ferrislib = require('../build/index')
 const token = "OTc5OTc1Mjc1MzkwMTQ3MjUxODc3NTk4OTg2MjQw.oPv_AsjrLgMwF4QD2BTWkvYGfZMopTvILZM7NVSAkvqI_dWNYlofF9RRbD_9wn4PYskwDXHhgNkPmjUpS3GlsRYeqxS3plXmWwlSqPWsLwPyvSJjrT-UD03cf3BXE0oX4eSgE9PyrQOQrypdBIIzjJoziPLC2WpwnANuPVtqkPlmSRS7nmsAJBhkStUzqOOfX9pZIoCl4knt1ppOalhYgvpZ8EmfX4Nbu31Oc331lOeOQUGJEuIo-0YHyEmBTOFqw0CIJcjaoGXfP8lO0G-vLClR_EIRLLMNEcyKYb1pWMp0FrfrzKMBTklvVRxePwRFHl0mzg7I7HCUxmRQZcM8Zw=="
 const sys = new Ferrislib.Client(token, {
     shardCount: 1,
