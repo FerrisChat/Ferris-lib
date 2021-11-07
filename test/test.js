6dec781ed6bd57522cc618b54f7850c88dc8ddb4
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2021-11-07 14:13:53 -0400

    Test Revamp

--- test/test.js
+++ test/test.js
@@ -1,25 +1,14 @@
 const Ferrislib = require('../build/index')
 const token = "MTA1NDc4Njk1NDUzODU2MjE4NzY3ODY5NDc2ODY0MA==.BVwW6vC4gw-B6MVJGqqaaGWIw1saNFvEOH7cSLbcmJb53lv-wv4mUHKlpy_c0PiJeXbR57dqMy77iv8a5VZd1g=="
-const sys = new Ferrislib.Client(token, {
+const client = new Ferrislib.Client(token, {
     shardCount: 1,
 })
 
-/** */
-async function test() {
-    sys.on("debug", console.log)
-    sys.on("ready", async () => {
-        console.log(`${sys.user.name} has logged in`)
-    })
-    sys.on("shardReconnecting", (id) => console.log(`Shard ${id} is reconnecting...`))
-    sys.on("shardReady", (id) => console.log(`Shard ${id} is ready`))
-    sys.on("shardDisconnected", (id, code, reason) => console.log(`Shard ${id} disconnected with the Code: ${code} and reason: ${reason}`))
-    sys.connect()
-}
-test()
+client.on("rawWs", console.log)
+client.on("debug", console.log)
+client.on("ready", async () => {
+    await client.user.fetch()
+    console.log(client.user.guilds)
+})
 
-
-setInterval(() => {
-    //console.log("Memory Usage (Rss)", Math.round((process.memoryUsage().rss / 1024 / 1024) * 100) / 100, "MB")
-    //console.log("Memory Usage (HeapUsed)", Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100, "MB")
-    console.log(`Average Latency: ${sys.ws.avgerageLatency}`)
-}, 1000 * 60)
+client.connect()
